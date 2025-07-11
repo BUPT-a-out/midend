62ef5f73ce910d5dbb51fbeb4fe93eb7e925a820
YouXam <youxam@outlook.com>
2025-07-11 22:01:22 +0800

    chore: migrate to xmake

---
+++ tests/xmake.lua
@@ -0,0 +1,36 @@
+add_requires("gtest 1.14.0", {configs = {shared = false, main = true}, system = false})
+
+target("midend_tests")
+    set_kind("binary")
+    set_languages("c++17")
+    set_default(false)
+    
+    add_packages("gtest")
+    
+    add_files("IR/*.cpp")
+    add_files("Pass/**/*.cpp")
+    add_files("Pass/*.cpp")
+    
+    add_deps("midend")
+    
+    add_cxxflags("-DGTEST_HAS_PTHREAD=1")
+    add_defines("GTEST_LINKED_AS_SHARED_LIBRARY=0")
+    
+    set_warnings("all")
+    add_cxxflags("-Wall", "-Wextra")
+    
+    if is_mode("debug") then
+        add_cxxflags("-g", "-O0")
+        set_symbols("debug")
+        set_optimize("none")
+    elseif is_mode("release") then
+        add_cxxflags("-O3", "-DNDEBUG")
+        set_symbols("hidden")
+        set_optimize("fastest")
+    end
+    
+    add_ldflags("-pthread")
+    
+    after_build(function (target)
+        print("Midend tests built successfully. Run with: xmake run midend_tests")
+    end)
---
+++ xmake.lua
@@ -0,0 +1,46 @@
+target("midend")
+    set_kind("static")
+    set_languages("c++17")
+    
+    add_files("src/IR/*.cpp")
+    add_files("src/IR/**/*.cpp")
+    add_files("src/Pass/*.cpp")
+    add_files("src/Pass/**/*.cpp")
+    
+    add_includedirs("include", {public = true})
+    
+    add_headerfiles("include/(IR/*.h)")
+    add_headerfiles("include/(IR/Instructions/*.h)")
+    add_headerfiles("include/(Pass/**/*.h)")
+    add_headerfiles("include/(Support/*.h)")
+    
+    set_warnings("all")
+    add_cxxflags("-Wall", "-Wextra")
+    
+    if is_mode("debug") then
+        add_cxxflags("-g", "-O0")
+        set_symbols("debug")
+        set_optimize("none")
+    elseif is_mode("release") then
+        add_cxxflags("-O3", "-DNDEBUG")
+        set_symbols("hidden")
+        set_optimize("fastest")
+    end
+
+if os.isdir(path.join(os.scriptdir(), "tests")) then
+    includes("tests/xmake.lua")
+end
+
+task("test")
+    set_menu {
+        usage = "xmake test",
+        description = "Run midend tests",
+        options = {}
+    }
+    on_run(function ()
+        import("core.project.project")
+        import("core.base.task")
+        
+        task.run("build", {}, "midend_tests")
+        os.exec("xmake run midend_tests")
+    end)
